cb2029fd32e5e835499ba91875850d6ee07e7f5d
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2019-02-20 10:48:14 +0100

    Fix typo in test script

--- testsuite/tests/lib-unix/unix-execvpe/has-execvpe.sh
+++ testsuite/tests/lib-unix/unix-execvpe/has-execvpe.sh
@@ -2,7 +2,7 @@
 
 # This script is related to the 'exec.ml' test.
 # It tests whether the OS implements execvpe or not.
-# It makes sense to run the tests only if execvpe is nt implemented.
+# It makes sense to run the tests only if execvpe is not implemented.
 # If it is implemented, the test is skipped.
 
 if grep -q "#define HAS_EXECVPE" ${ocamlsrcdir}/runtime/caml/s.h; then
