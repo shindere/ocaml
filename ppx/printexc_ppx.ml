19789a36c063597ba6b68cc27d72290283fa90a8
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2019-06-06 21:30:20 +0200

    Fix build of toy ppx

--- ppx/printexc_ppx.ml
+++ ppx/printexc_ppx.ml
@@ -3,18 +3,18 @@ module H = Ast_helper
 open Asttypes
 open Parsetree
 open Longident
-open Format
+(* open Format *)
 
 let super = M.default_mapper
 
 let extract_name arg =
   match arg.pexp_desc with
-  | Pexp_construct (name, _) -> Format.asprintf "%a" Printtyp.longident name.txt; 
+  | Pexp_construct (name, _) -> Format.asprintf "%a" Printtyp.longident name.txt;
   | _ -> ""
 
 let get_line_num arg =
   match arg.pexp_desc with
-  | Pexp_construct (name, _) -> Printf.sprintf "%d" name.loc.loc_start.pos_lnum; 
+  | Pexp_construct (name, _) -> Printf.sprintf "%d" name.loc.loc_start.pos_lnum;
   | _ -> ""
 
 let is_raise_id expr =
@@ -24,16 +24,16 @@ let is_raise_id expr =
 
 let option_is_raise expr =
   match expr.pexp_desc with
-  | Pexp_apply (fn, [_, constr]) -> if is_raise_id fn 
-      then Some ((extract_name constr) ^ " on line " ^ (get_line_num constr)) 
+  | Pexp_apply (fn, [_, constr]) -> if is_raise_id fn
+      then Some ((extract_name constr) ^ " on line " ^ (get_line_num constr))
       else None
   | _ -> None
 
 let create_raise_string exc_name =
    H.Exp.constant @@
-   H.Const.string ("raised " ^ exc_name) 
+   H.Const.string ("raised " ^ exc_name)
 
-let create_print str loc = H.Exp.apply 
+let create_print str loc = H.Exp.apply
       ( H.Exp.ident {txt = Lident "print_endline"; loc})
       ([Nolabel, str])
 
