d54b1ff8d036c0ae0db00243adf0f548c6275bf4
Sébastien Hinderer <seb@tarides.com>
2022-10-31 10:00:18 +0100

    Let the root Makefile control thelist of files ocntaining primitives
    Before this commit, the runtime/gen_primitives.sh script ocntained the
    list of C files to scan to build the table of primitives. With this commit,
    the script takes the list of files to scan as its arguments,, the list
    being controlled from the Makefile.

--- runtime/gen_primitives.sh
+++ runtime/gen_primitives.sh
@@ -20,17 +20,14 @@
 # #8985: the meaning of character range a-z depends on the locale, so force C
 #        locale throughout.
 export LC_ALL=C
+
+ints=$1
+shift
+
 (
-  for prim in \
-      alloc array compare extern floats gc_ctrl hash intern interp ints io \
-      lexing md5 meta memprof obj parsing signals str sys callback weak \
-      finalise domain platform fiber memory startup_aux runtime_events sync \
-      dynlink backtrace_byt backtrace afl \
-      bigarray prng
-  do
-      sed -n -e 's/^CAMLprim value \([a-z0-9_][a-z0-9_]*\).*/\1/p' \
-        "runtime/$prim.c"
+  for prim in $*; do \
+      sed -n -e 's/^CAMLprim value \([a-z0-9_][a-z0-9_]*\).*/\1/p' $prim
   done
   sed -n -e 's/^CAMLprim_int64_[0-9](\([a-z0-9_][a-z0-9_]*\)).*/caml_int64_\1\
-caml_int64_\1_native/p' runtime/ints.c
+caml_int64_\1_native/p' $ints
 ) | sort | uniq
