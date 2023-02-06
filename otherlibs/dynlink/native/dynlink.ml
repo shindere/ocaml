c0f04fe710ea29229395d6d3ca711d519520d210
Sébastien Hinderer <seb@tarides.com>
2023-02-06 16:56:24 +0100

    Define file formats for dynamically loadable files in stdlib
    This commit introduces the camlinternalDynlink module, containing
    everything needed to load cmo, cma and cmxs files dynamically.
    
    The commit also modifies the dynlink other library to rely on this new module
    and thus depend only on the standard library, rather than on compilerlibs
    as before.

--- otherlibs/dynlink/native/dynlink.ml
+++ otherlibs/dynlink/native/dynlink.ml
@@ -20,8 +20,6 @@
 
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-open! Dynlink_compilerlibs
-
 module DC = Dynlink_common
 module DT = Dynlink_types
 
@@ -35,7 +33,7 @@ type global_map = {
 module Native = struct
   type handle
 
-  external ndl_open : string -> bool -> handle * Cmxs_format.dynheader
+  external ndl_open : string -> bool -> handle * CamlinternalDynlink.dynheader
     = "caml_natdynlink_open"
   external ndl_run : handle -> string -> unit = "caml_natdynlink_run"
   external ndl_getmap : unit -> global_map list = "caml_natdynlink_getmap"
@@ -43,7 +41,7 @@ module Native = struct
   external ndl_loadsym : string -> Obj.t = "caml_natdynlink_loadsym"
 
   module Unit_header = struct
-    type t = Cmxs_format.dynunit
+    type t = CamlinternalDynlink.dynunit
 
     let name (t : t) = t.dynu_name
     let crc (t : t) = Some t.dynu_crc
@@ -93,7 +91,7 @@ module Native = struct
       try ndl_open filename (not priv)
       with exn -> raise (DT.Error (Cannot_open_dynamic_library exn))
     in
-    if header.dynu_magic <> Config.cmxs_magic_number then begin
+    if header.dynu_magic <> CamlinternalDynlink.cmxs_magic_number then begin
       raise (DT.Error (Not_a_bytecode_file filename))
     end;
     handle, header.dynu_units
